98bc9c023dd8fb71a6ad9b01cb9e8a60f6c8caba
Sebastian <sebastian.rytel@gmail.com>
2021-11-21 14:27:40 +0100

    modification

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -124,11 +124,11 @@ AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
 AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
-if config("DEBUG"):
+if config("DEBUG") == "TRUE":
     STATIC_ROOT = BASE_DIR / "static"
     STATIC_URL = '/static/'
     STATICFILES_DIRS = [
         BASE_DIR / "staticfiles",
     ]
-else:
+elif config("DEBUG") == "FALSE":
     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
